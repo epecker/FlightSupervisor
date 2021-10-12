77aeddc12c626462307dd72ce1b486f60a2a183b
trautrim.tbt@outlook.com <trautrim.tbt@outlook.com>
2021-10-12 18:29:09 +0000

    Merged in update_repo (pull request #5)
    feat (Repo.hpp): Additions for pilot_control state

--- atomic_models/Repo.hpp
+++ atomic_models/Repo.hpp
@@ -30,6 +30,7 @@ using namespace std;
 struct Repo_defs {
 	struct lp_new_in : public in_port<Message_t> {};
 	struct lp_crit_met_in : public in_port<Message_t> {};
+	struct pilot_takeover_in : public in_port<Message_t> {};
 
 	struct lp_repo_new_out : public out_port<Message_t> {};
 	struct pilot_handover_out : public out_port<Message_t> {};
@@ -43,18 +44,19 @@ public:
 	// (not required for the simulator)
 	DEFINE_ENUM_WITH_STRING_CONVERSIONS(States,
 		(IDLE)
+		(LAND)
 		(LP_REPO)
 		(NEW_LP_REPO)
 		(NOTIFY_LAND)
-		(LAND)
-		(LP_HOVER)
+		(PILOT_CONTROL)
 	)
 
-	// Create a tuple of input ports (required for the simulator)
-	using input_ports = tuple<
+		// Create a tuple of input ports (required for the simulator)
+		using input_ports = tuple<
 		typename Repo_defs::lp_crit_met_in,
-		typename Repo_defs::lp_new_in
-	>;
+		typename Repo_defs::lp_new_in,
+		typename Repo_defs::pilot_takeover_in
+		>;
 
 	// Create a tuple of output ports (required for the simulator)
 	using output_ports = tuple<
@@ -75,7 +77,7 @@ public:
 	// Default constructor
 	Repo() {
 		state.current_state = IDLE;
-		state.next_internal = TIME(LP_REPOSITION_TIME);
+		state.next_internal = numeric_limits<TIME>::infinity();
 	}
 
 	// Internal transitions
@@ -88,12 +90,12 @@ public:
 				state.next_internal = TIME(LP_REPOSITION_TIME);
 				break;
 			case LP_REPO:
-				state.current_state = LP_HOVER;
-				state.next_internal = std::numeric_limits<TIME>::infinity();
+				state.current_state = PILOT_CONTROL;
+				state.next_internal = numeric_limits<TIME>::infinity();
 				break;
 			case NOTIFY_LAND:
 				state.current_state = LAND;
-				state.next_internal = std::numeric_limits<TIME>::infinity();
+				state.next_internal = numeric_limits<TIME>::infinity();
 				break;
 			default:
 				break;
@@ -106,20 +108,38 @@ public:
 	void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
 		bool received_lp_new;
 		bool received_lp_crit_met;
+		bool received_pilot_takeover;
 
 		switch (state.current_state) {
 			case IDLE:
 				received_lp_new = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
-				if (received_lp_new) {
+				received_pilot_takeover = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
+
+				if (received_pilot_takeover) {
+					state.current_state = PILOT_CONTROL;
+					state.next_internal = numeric_limits<TIME>::infinity();
+				} else if (received_lp_new) {
 					state.current_state = LP_REPO;
 					state.next_internal = TIME(LP_REPOSITION_TIME);
+				}
+				break;
+			case LAND:
+				received_pilot_takeover = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
+
+				if (received_pilot_takeover) {
+					state.current_state = PILOT_CONTROL;
+					state.next_internal = numeric_limits<TIME>::infinity();
 				}
 				break;
 			case LP_REPO:
 				received_lp_new = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
 				received_lp_crit_met = get_messages<typename Repo_defs::lp_crit_met_in>(mbs).size() >= 1;
-				// Receiving a new landing point takes precedence over meeting the landing criteria 
-				if (received_lp_new) {
+				received_pilot_takeover = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
+
+				if (received_pilot_takeover) {
+					state.current_state = PILOT_CONTROL;
+					state.next_internal = numeric_limits<TIME>::infinity();
+				} else if (received_lp_new) {
 					vector<Message_t> new_landing_points = get_messages<typename Repo_defs::lp_new_in>(mbs);
 					state.landing_point = new_landing_points[0]; // set the new Landing 
 					state.current_state = NEW_LP_REPO;
@@ -127,6 +147,22 @@ public:
 				} else if (received_lp_crit_met) {
 					state.current_state = NOTIFY_LAND;
 					state.next_internal = TIME("00:00:00:000");
+				}
+				break;
+			case NEW_LP_REPO:
+				received_pilot_takeover = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
+
+				if (received_pilot_takeover) {
+					state.current_state = PILOT_CONTROL;
+					state.next_internal = numeric_limits<TIME>::infinity();
+				}
+				break;
+			case NOTIFY_LAND:
+				received_pilot_takeover = get_messages<typename Repo_defs::lp_new_in>(mbs).size() >= 1;
+
+				if (received_pilot_takeover) {
+					state.current_state = PILOT_CONTROL;
+					state.next_internal = numeric_limits<TIME>::infinity();
 				}
 				break;
 			default:
@@ -138,7 +174,7 @@ public:
 	// Used to call set call precedence
 	void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
 		internal_transition();
-		external_transition(TIME(), std::move(mbs));
+		external_transition(TIME(), move(mbs));
 	}
 
 	// output function
@@ -151,7 +187,7 @@ public:
 				bag_port_out.push_back(state.landing_point);
 				get_messages<typename Repo_defs::land_out>(bags) = bag_port_out;
 				break;
-			case LP_HOVER:
+			case PILOT_CONTROL:
 				bag_port_out.push_back(state.landing_point);
 				get_messages<typename Repo_defs::pilot_handover_out>(bags) = bag_port_out;
 				break;
