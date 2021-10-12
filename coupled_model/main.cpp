beae0a6aa0dc54c200007147429a082e128ce247
James_WE_Horner <jameshorner@cmail.carleton.ca>
2021-10-12 18:19:33 -0400

    Added missing code to the main function for running.

--- coupled_model/main.cpp
+++ coupled_model/main.cpp
@@ -166,5 +166,28 @@ int main(int argc, char* argv[]) {
 		"TestDriver", submodels_TestDriver, iports_TestDriver, oports_TestDriver, eics_TestDriver, eocs_TestDriver, ics_TestDriver
 	);
 
+	/*************** Loggers *******************/
+	static ofstream out_messages("../simulation_results/output_messages.txt");
+	struct oss_sink_messages {
+		static ostream& sink() {
+			return out_messages;
+		}
+	};
+	static ofstream out_state("../simulation_results/output_state.txt");
+	struct oss_sink_state {
+		static ostream& sink() {
+			return out_state;
+		}
+	};
+
+	using state = logger::logger<logger::logger_state, dynamic::logger::formatter<TIME>, oss_sink_state>;
+	using log_messages = logger::logger<logger::logger_messages, dynamic::logger::formatter<TIME>, oss_sink_messages>;
+	using global_time_mes = logger::logger<logger::logger_global_time, dynamic::logger::formatter<TIME>, oss_sink_messages>;
+	using global_time_sta = logger::logger<logger::logger_global_time, dynamic::logger::formatter<TIME>, oss_sink_state>;
+
+	using logger_supervisor = logger::multilogger<state, log_messages, global_time_mes, global_time_sta>;
+
+	dynamic::engine::runner<NDTime, logger_supervisor> r(TEST_DRIVER, { 0 });
+	r.run_until_passivate();
 	return 0;
 }
