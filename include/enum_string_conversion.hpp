7cb341c87a552ac734a55f47e83305cd2fb9b2b7
ttrautrim <trautrim.tbt@outlook.com>
2021-11-28 17:19:34 -0500

    feat (stringToEnum): Takes a string and returns the corresponding enum

--- include/enum_string_conversion.hpp
+++ include/enum_string_conversion.hpp
@@ -6,6 +6,9 @@
 #define X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE(r, data, elem)    \
     case data::elem : return BOOST_PP_STRINGIZE(elem);
 
+#define X_DEFINE_STRING_TO_ENUM_CONVERSIONS(r, data, elem)                    \
+    if (s.compare(BOOST_PP_STRINGIZE(elem)) == 0) return data::elem;
+
 #define DEFINE_ENUM_WITH_STRING_CONVERSIONS(name, enumerators)                \
     enum class name {                                                         \
         BOOST_PP_SEQ_ENUM(enumerators)                                        \
@@ -22,5 +25,15 @@
             )                                                                 \
             default: return "[Unknown " BOOST_PP_STRINGIZE(name) "]";         \
         }                                                                     \
+    }                                                                         \
+                                                                              \
+    inline const static name stringToEnum(string s)                           \
+    {                                                                         \
+            BOOST_PP_SEQ_FOR_EACH(                                            \
+                X_DEFINE_STRING_TO_ENUM_CONVERSIONS,                          \
+                name,                                                         \
+                enumerators                                                   \
+            )                                                                 \
+            return name::BOOST_PP_SEQ_ELEM(0, enumerators);                   \
     }
 #endif // ENUM_STRING_CONVERSION_HPP
