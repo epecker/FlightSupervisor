97c5e8ac427c501c5723508e4d9f0c7c57b00c6a
James Horner <jwehorner@gmail.com>
2022-06-13 15:37:13 -0400

    (Feat) Changed asynchronous input model to receive UDP packets (in-progress).

---
+++ include/io_models/UDP_Input_Async.hpp
@@ -0,0 +1,247 @@
+/**
+ *	\brief		An atomic model for receiving UDP packets on a specified port.
+ *	\details	This header file defines an asynchronous input atomic model
+				(without polling) for use in the RT-Cadmium DEVS simulation 
+				software. 
+ *	\author		James Horner
+ */
+
+#ifndef UDP_INPUT_ASYNC_HPP
+#define UDP_INPUT_ASYNC_HPP
+
+// System libraries
+#include <iostream>
+#include <assert.h>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <chrono>
+#include <sstream>
+
+#include <boost/asio.hpp>
+
+// RT-Cadmium
+#include <cadmium/engine/pdevs_dynamic_runner.hpp>
+#include <cadmium/modeling/ports.hpp>
+#include <cadmium/modeling/message_bag.hpp>
+#include <cadmium/modeling/dynamic_model.hpp>
+
+// Message Structures
+#include "message_structures/message_command_ack_t.hpp"
+
+// Includes
+#include "enum_string_conversion.hpp"
+#include "Constants.hpp"
+// #include "UDP_Callback.hpp"
+
+#ifdef RT_LINUX
+
+using namespace cadmium;
+using namespace std;
+using namespace boost;
+
+// Global mutexes for thread synchronization using unique locks.
+std::mutex input_mutex;
+
+// Input and output port definitions
+template<typename MSG>
+struct UDP_Input_Async_defs {
+	struct o_message : public out_port<MSG> { };
+	struct i_quit : public in_port<bool> { };
+};
+
+// Atomic model
+template<typename MSG, typename TIME>
+class UDP_Input_Async {
+
+	// Private members for thread management.
+private:
+	cadmium::dynamic::modeling::AsyncEventSubject* _sub;
+	
+	mutable std::vector<MSG> message;
+	asio::ip::udp::endpoint network_endpoint;
+	asio::ip::udp::endpoint remote_endpoint;
+	asio::io_service io_service;
+	asio::ip::udp::socket socket{ io_service };
+	bool send_ack;
+	char recv_buffer[MAX_SER_BUFFER_CHARS];
+
+public:
+	// Used to keep track of the states
+	// (not required for the simulator)
+	DEFINE_ENUM_WITH_STRING_CONVERSIONS(States,
+		(IDLE)
+		(INPUT)
+	);
+
+	//Parameters to be overwriten when instantiating the atomic model
+	//Callback_Tester* tester;
+
+	// Default Constructor
+	UDP_Input_Async() {
+		//Initialise the current state
+		state.current_state = States::INPUT;
+		state.has_messages = false;
+
+		//Create the network endpoint using a default address and port.
+		send_ack = false;
+		unsigned short port_num = (unsigned short)MAVLINK_OVER_UDP_PORT;
+		network_endpoint = asio::ip::udp::endpoint(asio::ip::address::from_string(PEREGRINE_IP), port_num);
+
+		//Start the user input thread.
+		std::thread(&UDP_Input_Async::receive_packet_thread, this).detach();
+	}
+
+	//Constructor with address and port as well as whether acknowledgements are required.
+	UDP_Input_Async(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) {
+		//Initialise the current state
+		state.current_state = States::INPUT;
+		state.has_messages = false;
+		_sub = sub;
+
+		//Create the network endpoint using the supplied address and port.
+		send_ack = false;
+		unsigned short port_num = (unsigned short)strtoul(port.c_str(), NULL, 0);
+		network_endpoint = asio::ip::udp::endpoint(asio::ip::address::from_string(address), port_num);
+
+		//Start the user input thread.
+		std::thread(&UDP_Input_Async::receive_packet_thread, this).detach();
+	}
+
+	// Destructor for the class that stops the packet receipt thread
+	~UDP_Input_Async() {
+		//Before exiting stop the Boost IO service to interupt the receipt handler.
+		io_service.stop();
+	}
+
+	// This is used to track the state of the atomic model. 
+	// (required for the simulator)
+	struct state_type {
+		States current_state;
+		bool has_messages;
+	};
+	state_type state;
+
+	// Create a tuple of input ports (required for the simulator)
+	using input_ports = std::tuple<typename UDP_Input_Async_defs<MSG>::i_quit>;
+
+	// Create a tuple of output ports (required for the simulator)
+	using output_ports = std::tuple<typename UDP_Input_Async_defs<MSG>::o_message>;
+
+	// Internal transitions
+	// These are transitions occuring from internal inputs
+	// (required for the simulator)
+	void internal_transition() {
+		std::unique_lock<std::mutex> mutexLock(input_mutex, std::defer_lock);
+		//If the thread has finished receiving input, change state if there are messages.
+		if (mutexLock.try_lock()) {
+			state.has_messages = !message.empty();
+		}
+	}
+
+	// External transitions
+	// These are transitions occuring from external inputs
+	// (required for the simulator)
+	void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
+		state.has_messages = !message.empty();
+		if (get_messages<typename UDP_Input_Async_defs<MSG>::i_quit>(mbs).size() >= 1) {
+			state.current_state = States::IDLE;
+		}
+	}
+
+	// Confluence transition
+	// Used to call set call precedence
+	void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
+		internal_transition();
+		external_transition(TIME(), std::move(mbs));
+	}
+
+	// Output function
+	typename make_message_bags<output_ports>::type output() const {
+		typename make_message_bags<output_ports>::type bags;
+		vector<MSG> message_out;
+		std::unique_lock<std::mutex> mutexLock(input_mutex, std::defer_lock);
+		if (state.current_state == States::INPUT) {
+			//If the lock is free and there are messages, send the messages.
+			if (state.has_messages && mutexLock.try_lock()) {
+				for (auto& msg : message) {
+					message_out.push_back(msg);
+				}
+				message.clear();
+				get_messages<typename UDP_Input_Async_defs<MSG>::o_message>(bags) = message_out;
+			}
+		}
+		return bags;
+	}
+
+	// Time advance
+	// Used to set the internal time of the current state
+	TIME time_advance() const {
+		if (state.has_messages && state.current_state == States::INPUT) {
+			return TIME("00:00:00:00");
+		}
+
+		return std::numeric_limits<TIME>::infinity();
+	}
+
+	// Child thread for receiving UDP packets
+	void receive_packet_thread() {
+		//Open and bind the socket using Boost.
+		socket.open(asio::ip::udp::v4());
+		socket.bind(network_endpoint);
+
+		//While the model is not passivated,
+		while (state.current_state != States::IDLE) {
+			//Reset the io service then asynchronously receive a packet and 
+			//use the handler to add it to the message vector.
+			io_service.reset();
+			socket.async_receive_from(
+				asio::buffer(recv_buffer),
+				remote_endpoint,
+				bind(
+				&UDP_Input_Async::receive_packet,
+				this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+
+			//Receive one packet then loop.
+			io_service.run_one();
+			std::cout << "Calling notify." << std::endl;
+			_sub->notify();
+		}
+		//Once done, close the socket.
+		socket.close();
+	}
+
+	//Message handler that is called on UDP packet receipt.
+	void receive_packet(const boost::system::error_code& error, size_t bytes_transferred) {
+		//Aquire the unique lock for the message vector.
+		std::unique_lock<std::mutex> mutexLock(input_mutex);
+		if (error) return;
+
+		//Add the message to the vector.
+		MSG recv = MSG();
+		memcpy(&recv, &recv_buffer, bytes_transferred);
+		message.insert(message.begin(), recv);
+
+		//If an ack is required,
+		if (send_ack) {
+			//Construct the ack message and associated data array.
+			message_command_ack_t ack_message(MAV_CMD_DEFAULT, MAV_RESULT_ACCEPTED, 0, 0, 0, 0);
+			boost::system::error_code ack_err;
+			char ack_data[sizeof(message_command_ack_t)];
+			memcpy(ack_data, &ack_message, sizeof(ack_data));
+
+			//Send the ack to the origin of the packet.
+			socket.send_to(asio::buffer(ack_data), remote_endpoint, 0, ack_err);
+		}
+	}
+
+	friend std::ostringstream& operator<<(std::ostringstream& os, const typename UDP_Input_Async<MSG, TIME>::state_type& i) {
+		os << "State: " << enumToString(i.current_state) << "-" << (i.has_messages ? "MESSAGES" : "NO_MESSAGES");
+		return os;
+	}
+};
+
+#endif // RT_LINUX
+#endif // UDP_INPUT_ASYNC_HPP
--- test/drivers/td_udp_input_async.cpp
+++ test/drivers/td_udp_input_async.cpp
@@ -27,12 +27,13 @@
 #include "input_readers.hpp" // Input Reader Definitions.
 
 //Coupled model headers
-#include "io_models/Async_Test.hpp"
+#include "io_models/UDP_Input_Async.hpp"
+#include "io_models/UDP_Output.hpp"
 
 using namespace std;
 using namespace cadmium;
 
-using hclock = chrono::high_resolution_clock;
+using hclock = std::chrono::high_resolution_clock;
 using TIME = NDTime;
 
 // Used for oss_sink_state and oss_sink_messages
@@ -41,19 +42,33 @@ ofstream out_state;
 ofstream out_info;
 
 // Define output ports to be used for logging purposes
-struct out : public out_port<int> {};
-struct delay : public out_port<bool> {};
+struct out : public out_port<message_aircraft_state_t> {};
+
+template<typename T>
+class UDP_Output_LP : public UDP_Output<message_landing_point_t, T> {
+public:
+    UDP_Output_LP() = default;
+    UDP_Output_LP(string address, string port) : UDP_Output<message_landing_point_t, T>(address, port){}
+};
+
+template<typename T>
+class UDP_Input_Async_LP : public UDP_Input_Async<message_landing_point_t, T> {
+public:
+    UDP_Input_Async_LP() = default;
+    UDP_Input_Async_LP(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<message_landing_point_t, T>(sub, ack_required, address, port){}
+};
 
 int main(int argc, char* argv[]) {
 	int test_set_enumeration = 0;
 
-	const string i_base_dir = string(PROJECT_DIRECTORY) + string("/test/input_data/async_test/");
-	const string o_base_dir = string(PROJECT_DIRECTORY) + string("/test/simulation_results/async_test/");
+	const string i_base_dir = string(PROJECT_DIRECTORY) + string("/test/input_data/udp_input_async/");
+	const string o_base_dir = string(PROJECT_DIRECTORY) + string("/test/simulation_results/udp_input_async/");
 
 	do {
 		// Input Files
 		string input_dir = i_base_dir + to_string(test_set_enumeration);
-		string input_file_in = input_dir + string("/delay.txt");
+		string input_file_message = input_dir + string("/message.txt");
+		string input_file_quit = input_dir + string("/quit.txt");
 
 		// Output locations
 		string out_directory = o_base_dir + to_string(test_set_enumeration);
@@ -61,7 +76,8 @@ int main(int argc, char* argv[]) {
 		string out_state_file = out_directory + string("/output_state.txt");
 		string out_info_file = out_directory + string("/output_info.txt");
 
-		if (!filesystem::exists(input_file_in)) {
+		if (!filesystem::exists(input_file_message) ||
+			!filesystem::exists(input_file_quit)) {
 			printf("One of the input files do not exist\n");
 			return 1;
 		}
@@ -70,40 +86,43 @@ int main(int argc, char* argv[]) {
 		filesystem::create_directories(out_directory.c_str()); // Creates if it does not exist. Does nothing if it does.
 
 		// Instantiate the atomic model to test
-		shared_ptr<dynamic::modeling::model> async_test = dynamic::translate::make_dynamic_atomic_model<Async_Test, TIME>("async_test");
+		std::shared_ptr<dynamic::modeling::model> udp_output = dynamic::translate::make_dynamic_atomic_model<UDP_Output_LP, TIME, const char*, const char*>("udp_output", std::move("127.0.0.1"), std::move("23000"));
+		std::shared_ptr<dynamic::modeling::model> udp_input_async = dynamic::translate::make_dynamic_asynchronus_atomic_model<UDP_Input_Async_LP, TIME, bool, const char*, const char*>("udp_input_async", true, std::move("127.0.0.1"), std::move("23000"));
 
 		// Instantiate the input readers.
 		// One for each input
-		std::shared_ptr<dynamic::modeling::model> ir_delay =
-			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Boolean, TIME, const char* >("ir_delay", std::move(input_file_in.c_str()));
+		std::shared_ptr<dynamic::modeling::model> ir_message =
+			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Mavlink_Mission_Item, TIME, const char* >("ir_message", std::move(input_file_message.c_str()));
+		std::shared_ptr<dynamic::modeling::model> ir_quit =
+			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Boolean, TIME, const char* >("ir_quit", std::move(input_file_quit.c_str()));
 
 		// The models to be included in this coupled model 
 		// (accepts atomic and coupled models)
 		dynamic::modeling::Models submodels_TestDriver = {
-			async_test,
-			ir_delay
+			udp_output,
+			udp_input_async,
+			ir_message,
+            ir_quit
 		};
 
 		dynamic::modeling::Ports iports_TestDriver = {	};
 
 		dynamic::modeling::Ports oports_TestDriver = {
-			typeid(out),
-			typeid(delay)
+			typeid(out)
 		};
 
 		dynamic::modeling::EICs eics_TestDriver = {	};
 
 		// The output ports will be used to export in logging
-		dynamic::modeling::EOCs eocs_TestDriver = {
-			dynamic::translate::make_EOC<Async_Test_defs::out, out>("async_test"),
-			dynamic::translate::make_EOC<iestream_input_defs<bool>::out, delay>("ir_delay")
-		};
+		dynamic::modeling::EOCs eocs_TestDriver = {	};
 
 		// This will connect our outputs from our input reader to the file
 		dynamic::modeling::ICs ics_TestDriver = {
+			dynamic::translate::make_IC<iestream_input_defs<message_landing_point_t>::out, UDP_Output_defs<message_landing_point_t>::i_message>("ir_message", "udp_output"),
+			dynamic::translate::make_IC<iestream_input_defs<bool>::out, UDP_Input_Async_defs<message_landing_point_t>::i_quit>("ir_quit", "udp_input_async")
 		};
 
-		shared_ptr<dynamic::modeling::coupled<TIME>> test_driver = make_shared<dynamic::modeling::coupled<TIME>>(
+		std::shared_ptr<dynamic::modeling::coupled<TIME>> test_driver = std::make_shared<dynamic::modeling::coupled<TIME>>(
 			"test_driver", submodels_TestDriver, iports_TestDriver, oports_TestDriver, eics_TestDriver, eocs_TestDriver, ics_TestDriver
 		);
 
@@ -150,12 +169,12 @@ int main(int argc, char* argv[]) {
 	fflush(NULL);
 	string path_to_script = PROJECT_DIRECTORY + string("/test/scripts/simulation_cleanup.py");
 	string path_to_simulation_results = PROJECT_DIRECTORY + string("/test/simulation_results");
-	if (system("python3 --version") == 0) {
+	if (std::system("python3 --version") == 0) {
 		string command = "python3 " + path_to_script + string(" ") + path_to_simulation_results;
-		system(command.c_str());
-	} else if (system("python --version") == 0) {
+		std::system(command.c_str());
+	} else if (std::system("python --version") == 0) {
 		string command = "python " + path_to_script + string(" ") + path_to_simulation_results;
-		system(command.c_str());
+		std::system(command.c_str());
 	} else {
 		cout << "\nPython is not installed!\n";
 	}
