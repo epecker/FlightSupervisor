a948a7ba0aa98de1bf935fe5bea26c3ffbb161b4
James Horner <jwehorner@gmail.com>
2022-06-22 16:52:29 -0400

    (Feat) Started integration by defining input models and cleaning up RT Supervisor file (In Progress).

---
+++ include/input_models.hpp
@@ -0,0 +1,285 @@
+/**
+ *	\brief		File defining commonly used input models for the Supervisor.
+ *	\details	This header file defines commonly used input models for use
+				in the integration of the Supervisor. The file defines models
+				for all the message structures as well as primitive data-types.
+ *	\author		James Horner
+ */
+
+#ifndef INPUT_MODELS_HPP
+#define INPUT_MODELS_HPP
+
+// Basic models for receiving inputs.
+#include "io_models/UDP_Input.hpp"
+#include "io_models/UDP_Input_Async.hpp"
+#include "io_models/Shared_Memory_Input.hpp"
+
+// Messages structures
+#include "message_structures/message_aircraft_state_t.hpp"
+#include "message_structures/message_command_ack_t.hpp"
+#include "message_structures/message_fcc_command_t.hpp"
+#include "message_structures/message_hover_criteria_t.hpp"
+#include "message_structures/message_landing_point_t.hpp"
+
+using namespace cadmium::basic_models::pdevs;
+
+/**
+* ==========================================================
+* ASYNCHRONOUS UDP INPUT MODELS
+* ==========================================================
+*/
+
+// Aircraft State
+template<typename T>
+class UDP_Input_Async_Aircraft_State : public UDP_Input_Async<message_aircraft_state_t, T> {
+public:
+	UDP_Input_Async_Aircraft_State() = default;
+	UDP_Input_Async_Aircraft_State(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<message_aircraft_state_t, T>(sub, ack_required, address, port) {};
+};
+
+// Boolean
+template<typename T>
+class UDP_Input_Async_Boolean : public UDP_Input_Async<bool, T> {
+public:
+	UDP_Input_Async_Boolean() = default;
+	UDP_Input_Async_Boolean(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<bool, T>(sub, ack_required, address, port) {};
+};
+
+// Command ACK
+template<typename T>
+class UDP_Input_Async_Command_Ack : public UDP_Input_Async<message_command_ack_t, T> {
+public:
+	UDP_Input_Async_Command_Ack() = default;
+	UDP_Input_Async_Command_Ack(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<message_command_ack_t, T>(sub, ack_required, address, port) {};
+};
+
+// Double
+template<typename T>
+class UDP_Input_Async_Double : public UDP_Input_Async<double, T> {
+public:
+	UDP_Input_Async_Double() = default;
+	UDP_Input_Async_Double(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<double, T>(sub, ack_required, address, port) {};
+};
+
+// FCC Command
+template<typename T>
+class UDP_Input_Async_Fcc_Command : public UDP_Input_Async<message_fcc_command_t, T> {
+public:
+	UDP_Input_Async_Fcc_Command() = default;
+	UDP_Input_Async_Fcc_Command(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<message_fcc_command_t, T>(sub, ack_required, address, port) {};
+};
+
+// Float
+template<typename T>
+class UDP_Input_Async_Float : public UDP_Input_Async<float, T> {
+public:
+	UDP_Input_Async_Float() = default;
+	UDP_Input_Async_Float(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<float, T>(sub, ack_required, address, port) {};
+};
+
+// Hover Criteria Message
+template<typename T>
+class UDP_Input_Async_Hover_Criteria : public UDP_Input_Async<message_hover_criteria_t, T> {
+public:
+	UDP_Input_Async_Hover_Criteria() = default;
+	UDP_Input_Async_Hover_Criteria(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<message_hover_criteria_t, T>(sub, ack_required, address, port) {};
+};
+
+// Int
+template<typename T>
+class UDP_Input_Async_Int : public UDP_Input_Async<int, T> {
+public:
+	UDP_Input_Async_Int() = default;
+	UDP_Input_Async_Int(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<int, T>(sub, ack_required, address, port) {};
+};
+
+// Landing Point
+template<typename T>
+class UDP_Input_Async_Landing_Point : public UDP_Input_Async<message_landing_point_t, T> {
+public:
+	UDP_Input_Async_Landing_Point() = default;
+	UDP_Input_Async_Landing_Point(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<message_landing_point_t, T>(sub, ack_required, address, port) {};
+};
+
+// String
+template<typename T>
+class UDP_Input_Async_String : public UDP_Input_Async<string, T> {
+public:
+	UDP_Input_Async_String() = default;
+	UDP_Input_Async_String(cadmium::dynamic::modeling::AsyncEventSubject* sub, bool ack_required, string address, string port) : UDP_Input_Async<string, T>(sub, ack_required, address, port) {};
+};
+
+
+/**
+* ==========================================================
+* POLLING UDP INPUT MODELS
+* ==========================================================
+*/
+
+// Aircraft State
+template<typename T>
+class UDP_Input_Aircraft_State : public UDP_Input<message_aircraft_state_t, T> {
+public:
+	UDP_Input_Aircraft_State() = default;
+	UDP_Input_Aircraft_State(T rate, bool ack_required, string address, string port) : UDP_Input<message_aircraft_state_t, T>(rate, ack_required, address, port) {};
+};
+
+// Boolean
+template<typename T>
+class UDP_Input_Boolean : public UDP_Input<bool, T> {
+public:
+	UDP_Input_Boolean() = default;
+	UDP_Input_Boolean(T rate, bool ack_required, string address, string port) : UDP_Input<bool, T>(rate, ack_required, address, port) {};
+};
+
+// Command ACK
+template<typename T>
+class UDP_Input_Command_Ack : public UDP_Input<message_command_ack_t, T> {
+public:
+	UDP_Input_Command_Ack() = default;
+	UDP_Input_Command_Ack(T rate, bool ack_required, string address, string port) : UDP_Input<message_command_ack_t, T>(rate, ack_required, address, port) {};
+};
+
+// Double
+template<typename T>
+class UDP_Input_Double : public UDP_Input<double, T> {
+public:
+	UDP_Input_Double() = default;
+	UDP_Input_Double(T rate, bool ack_required, string address, string port) : UDP_Input<double, T>(rate, ack_required, address, port) {};
+};
+
+// FCC Command
+template<typename T>
+class UDP_Input_Fcc_Command : public UDP_Input<message_fcc_command_t, T> {
+public:
+	UDP_Input_Fcc_Command() = default;
+	UDP_Input_Fcc_Command(T rate, bool ack_required, string address, string port) : UDP_Input<message_fcc_command_t, T>(rate, ack_required, address, port) {};
+};
+
+// Float
+template<typename T>
+class UDP_Input_Float : public UDP_Input<float, T> {
+public:
+	UDP_Input_Float() = default;
+	UDP_Input_Float(T rate, bool ack_required, string address, string port) : UDP_Input<float, T>(rate, ack_required, address, port) {};
+};
+
+// Hover Criteria Message
+template<typename T>
+class UDP_Input_Hover_Criteria : public UDP_Input<message_hover_criteria_t, T> {
+public:
+	UDP_Input_Hover_Criteria() = default;
+	UDP_Input_Hover_Criteria(T rate, bool ack_required, string address, string port) : UDP_Input<message_hover_criteria_t, T>(rate, ack_required, address, port) {};
+};
+
+// Int
+template<typename T>
+class UDP_Input_Int : public UDP_Input<int, T> {
+public:
+	UDP_Input_Int() = default;
+	UDP_Input_Int(T rate, bool ack_required, string address, string port) : UDP_Input<int, T>(rate, ack_required, address, port) {};
+};
+
+// Landing Point
+template<typename T>
+class UDP_Input_Landing_Point : public UDP_Input<message_landing_point_t, T> {
+public:
+	UDP_Input_Landing_Point() = default;
+	UDP_Input_Landing_Point(T rate, bool ack_required, string address, string port) : UDP_Input<message_landing_point_t, T>(rate, ack_required, address, port) {};
+};
+
+// String
+template<typename T>
+class UDP_Input_String : public UDP_Input<string, T> {
+public:
+	UDP_Input_String() = default;
+	UDP_Input_String(T rate, bool ack_required, string address, string port) : UDP_Input<string, T>(rate, ack_required, address, port) {};
+};
+
+/**
+* ==========================================================
+* POLLING SHARED MEMORY INPUT MODELS
+* ==========================================================
+*/
+
+// Aircraft State
+template<typename T>
+class Shared_Memory_Input_Aircraft_State : public Shared_Memory_Input<message_aircraft_state_t, T> {
+public:
+	Shared_Memory_Input_Aircraft_State() = default;
+	Shared_Memory_Input_Aircraft_State(T rate, string name) : Shared_Memory_Input<message_aircraft_state_t, T>(rate, name) {};
+};
+
+// Boolean
+template<typename T>
+class Shared_Memory_Input_Boolean : public Shared_Memory_Input<bool, T> {
+public:
+	Shared_Memory_Input_Boolean() = default;
+	Shared_Memory_Input_Boolean(T rate, string name) : Shared_Memory_Input<bool, T>(rate, name) {};
+};
+
+// Command ACK
+template<typename T>
+class Shared_Memory_Input_Command_Ack : public Shared_Memory_Input<message_command_ack_t, T> {
+public:
+	Shared_Memory_Input_Command_Ack() = default;
+	Shared_Memory_Input_Command_Ack(T rate, string name) : Shared_Memory_Input<message_command_ack_t, T>(rate, name) {};
+};
+
+// Double
+template<typename T>
+class Shared_Memory_Input_Double : public Shared_Memory_Input<double, T> {
+public:
+	Shared_Memory_Input_Double() = default;
+	Shared_Memory_Input_Double(T rate, string name) : Shared_Memory_Input<double, T>(rate, name) {};
+};
+
+// FCC Command
+template<typename T>
+class Shared_Memory_Input_Fcc_Command : public Shared_Memory_Input<message_fcc_command_t, T> {
+public:
+	Shared_Memory_Input_Fcc_Command() = default;
+	Shared_Memory_Input_Fcc_Command(T rate, string name) : Shared_Memory_Input<message_fcc_command_t, T>(rate, name) {};
+};
+
+// Float
+template<typename T>
+class Shared_Memory_Input_Float : public Shared_Memory_Input<float, T> {
+public:
+	Shared_Memory_Input_Float() = default;
+	Shared_Memory_Input_Float(T rate, string name) : Shared_Memory_Input<float, T>(rate, name) {};
+};
+
+// Hover Criteria Message
+template<typename T>
+class Shared_Memory_Input_Hover_Criteria : public Shared_Memory_Input<message_hover_criteria_t, T> {
+public:
+	Shared_Memory_Input_Hover_Criteria() = default;
+	Shared_Memory_Input_Hover_Criteria(T rate, string name) : Shared_Memory_Input<message_hover_criteria_t, T>(rate, name) {};
+};
+
+// Int
+template<typename T>
+class Shared_Memory_Input_Int : public Shared_Memory_Input<int, T> {
+public:
+	Shared_Memory_Input_Int() = default;
+	Shared_Memory_Input_Int(T rate, string name) : Shared_Memory_Input<int, T>(rate, name) {};
+};
+
+// Landing Point
+template<typename T>
+class Shared_Memory_Input_Landing_Point : public Shared_Memory_Input<message_landing_point_t, T> {
+public:
+	Shared_Memory_Input_Landing_Point() = default;
+	Shared_Memory_Input_Landing_Point(T rate, string name) : Shared_Memory_Input<message_landing_point_t, T>(rate, name) {};
+};
+
+// String
+template<typename T>
+class Shared_Memory_Input_String : public Shared_Memory_Input<string, T> {
+public:
+	Shared_Memory_Input_String() = default;
+	Shared_Memory_Input_String(T rate, string name) : Shared_Memory_Input<string, T>(rate, name) {};
+};
+
+#endif // INPUT_MODELS_HPP
--- src/rt_supervisor.cpp
+++ src/rt_supervisor.cpp
@@ -20,13 +20,9 @@
 //Time class header
 #include <NDTime.hpp>
 
-//Messages structures
-#include "message_structures/message_landing_point_t.hpp"
-#include "message_structures/message_fcc_command_t.hpp"
-
 // Project information headers this is created by cmake at generation time!!!!
 #include "SupervisorConfig.hpp"
-#include "input_readers.hpp" // Input Reader Definitions.
+#include "input_models.hpp" // Input Model Definitions.
 
 //Coupled model headers
 #include "coupled_models/Supervisor.hpp"
@@ -34,7 +30,7 @@
 using namespace std;
 using namespace cadmium;
 
-using hclock = chrono::high_resolution_clock;
+using hclock = std::chrono::high_resolution_clock;
 using TIME = NDTime;
 
 // Used for oss_sink_state and oss_sink_messages
@@ -54,145 +50,121 @@ struct o_fcc_command_hover : public out_port<message_fcc_command_t> {};
 
 
 int main(int argc, char* argv[]) {
-	int test_set_enumeration = 0;
+	const string o_base_dir = string(PROJECT_DIRECTORY) + string("/test/simulation_results/rt_supervisor");
 
-	const string i_base_dir = string(PROJECT_DIRECTORY) + string("/test/input_data/rt_supervisor/");
-	const string o_base_dir = string(PROJECT_DIRECTORY) + string("/test/simulation_results/rt_supervisor/");
+	string out_directory = o_base_dir;
+	string out_messages_file = out_directory + string("/output_messages.txt");
+	string out_state_file = out_directory + string("/output_state.txt");
+	string out_info_file = out_directory + string("/output_info.txt");
 
-	do {
-		// Input Files
-		string input_dir = i_base_dir + to_string(test_set_enumeration);
-		string input_file_landing_achieved = input_dir + string("/landing_achieved.txt");
-		string input_file_aircraft_state = input_dir + string("/aircraft_state.txt");
-		string input_file_pilot_takeover = input_dir + string("/pilot_takeover.txt");
-		string input_file_LP_recv = input_dir + string("/LP_recv.txt");
-		string input_file_PLP_ach = input_dir + string("/PLP_ach.txt");
+	// Create the output location
+	filesystem::create_directories(out_directory.c_str()); // Creates if it does not exist. Does nothing if it does.
 
-		// Output locations
-		string out_directory = o_base_dir + to_string(test_set_enumeration);
-		string out_messages_file = out_directory + string("/output_messages.txt");
-		string out_state_file = out_directory + string("/output_state.txt");
-		string out_info_file = out_directory + string("/output_info.txt");
+	// Instantiate the coupled model to test
+	Supervisor supervisor_object = Supervisor();
+	shared_ptr<dynamic::modeling::coupled<TIME>> supervisor = make_shared<dynamic::modeling::coupled<TIME>>("supervisor", supervisor_object.submodels, supervisor_object.iports, supervisor_object.oports, supervisor_object.eics, supervisor_object.eocs, supervisor_object.ics);
 
-		if (!filesystem::exists(input_file_landing_achieved) ||
-			!filesystem::exists(input_file_aircraft_state) ||
-			!filesystem::exists(input_file_pilot_takeover) ||
-			!filesystem::exists(input_file_LP_recv) ||
-			!filesystem::exists(input_file_PLP_ach)) {
-			printf("One of the input files do not exist\n");
-			return 1;
+	// Instantiate the input readers.
+	// One for each input
+	shared_ptr<dynamic::modeling::model> im_landing_achieved =
+		dynamic::translate::make_dynamic_atomic_model<UDP_Input_Boolean, TIME, TIME, bool, string, string>("im_landing_achieved", std::move(TIME("00:00:00:100")), true, std::move("127.0.0.1"), std::move("23001"));
+	shared_ptr<dynamic::modeling::model> im_pilot_takeover =
+		dynamic::translate::make_dynamic_atomic_model<UDP_Input_Boolean, TIME, TIME, bool, string, string>("im_pilot_takeover", std::move(TIME("00:00:00:100")), true, std::move("127.0.0.1"), std::move("23002"));
+	shared_ptr<dynamic::modeling::model> im_lp_recv =
+		dynamic::translate::make_dynamic_atomic_model<UDP_Input_Landing_Point, TIME, TIME, bool, string, string>("im_lp_recv", std::move(TIME("00:00:00:100")), true, std::move("127.0.0.1"), std::move("23003"));
+	shared_ptr<dynamic::modeling::model> im_plp_ach =
+		dynamic::translate::make_dynamic_atomic_model<UDP_Input_Landing_Point, TIME, TIME, bool, string, string>("im_plp_ach", std::move(TIME("00:00:00:100")), true, std::move("127.0.0.1"), std::move("23004"));
+
+	shared_ptr<dynamic::modeling::model> im_aircraft_state =
+		dynamic::translate::make_dynamic_atomic_model<Shared_Memory_Input_Aircraft_State, TIME, TIME, string>("im_aircraft_state", std::move(TIME("00:00:00:100")), std::move(DEFAULT_SHARED_MEMORY_NAME));
+
+	// The models to be included in this coupled model 
+	// (accepts atomic and coupled models)
+	dynamic::modeling::Models submodels_TestDriver = {
+		supervisor,
+		im_landing_achieved,
+		im_aircraft_state,
+		im_pilot_takeover,
+		im_lp_recv,
+		im_plp_ach
+	};
+
+	dynamic::modeling::Ports iports_TestDriver = { };
+
+	dynamic::modeling::Ports oports_TestDriver = {
+		typeid(o_LP_expired),
+		typeid(o_start_LZE_scan),
+		typeid(o_mission_complete),
+		typeid(o_land_requested),
+		typeid(o_fcc_command_velocity),
+		typeid(o_control_yielded),
+		typeid(o_notify_pilot),
+		typeid(o_fcc_command_hover)
+	};
+
+	dynamic::modeling::EICs eics_TestDriver = { };
+
+	// The output ports will be used to export in logging
+	dynamic::modeling::EOCs eocs_TestDriver = {
+		dynamic::translate::make_EOC<Supervisor_defs::o_LP_expired, o_LP_expired>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_start_LZE_scan, o_start_LZE_scan>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_mission_complete, o_mission_complete>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_land_requested, o_land_requested>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_fcc_command_velocity, o_fcc_command_velocity>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_control_yielded, o_control_yielded>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_notify_pilot, o_notify_pilot>("supervisor"),
+		dynamic::translate::make_EOC<Supervisor_defs::o_fcc_command_hover, o_fcc_command_hover>("supervisor")
+	};
+
+	// This will connect our outputs from our input reader to the file
+	dynamic::modeling::ICs ics_TestDriver = {
+		dynamic::translate::make_IC<UDP_Input_defs<bool>::out, Supervisor_defs::i_landing_achieved>("im_landing_achieved", "supervisor"),
+		dynamic::translate::make_IC<Shared_Memory_Input_defs<message_aircraft_state_t>::out, Supervisor_defs::i_aircraft_state>("im_aircraft_state", "supervisor"),
+		dynamic::translate::make_IC<UDP_Input_defs<bool>::out, Supervisor_defs::i_pilot_takeover>("im_pilot_takeover", "supervisor"),
+		dynamic::translate::make_IC<UDP_Input_defs<message_landing_point_t>::out, Supervisor_defs::i_LP_recv>("im_lp_recv", "supervisor"),
+		dynamic::translate::make_IC<UDP_Input_defs<message_landing_point_t>::out, Supervisor_defs::i_PLP_ach>("im_plp_ach", "supervisor")
+	};
+
+	shared_ptr<dynamic::modeling::coupled<TIME>> test_driver = make_shared<dynamic::modeling::coupled<TIME>>(
+		"test_driver", submodels_TestDriver, iports_TestDriver, oports_TestDriver, eics_TestDriver, eocs_TestDriver, ics_TestDriver
+	);
+
+	/*************** Loggers *******************/
+	out_messages = ofstream(out_messages_file);
+	struct oss_sink_messages {
+		static ostream& sink() {
+			return out_messages;
 		}
+	};
 
-		// Create the output location
-		filesystem::create_directories(out_directory.c_str()); // Creates if it does not exist. Does nothing if it does.
+	out_state = ofstream(out_state_file);
+	struct oss_sink_state {
+		static ostream& sink() {
+			return out_state;
+		}
+	};
 
-		// Instantiate the coupled model to test
-		Supervisor supervisor_object = Supervisor();
-		shared_ptr<dynamic::modeling::coupled<TIME>> supervisor = make_shared<dynamic::modeling::coupled<TIME>>("supervisor", supervisor_object.submodels, supervisor_object.iports, supervisor_object.oports, supervisor_object.eics, supervisor_object.eocs, supervisor_object.ics);
+	out_info = ofstream(out_info_file);
+	struct oss_sink_info {
+		static ostream& sink() {
+			return out_info;
+		}
+	};
 
-		// Instantiate the input readers.
-		// One for each input
-		shared_ptr<dynamic::modeling::model> ir_landing_achieved =
-			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Boolean, TIME, const char* >("ir_landing_achieved", move(input_file_landing_achieved.c_str()));
-		shared_ptr<dynamic::modeling::model> ir_aircraft_state =
-			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Aircraft_State, TIME, const char* >("ir_aircraft_state", move(input_file_aircraft_state.c_str()));
-		shared_ptr<dynamic::modeling::model> ir_pilot_takeover =
-			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Boolean, TIME, const char* >("ir_pilot_takeover", move(input_file_pilot_takeover.c_str()));
-		shared_ptr<dynamic::modeling::model> ir_lp_recv =
-			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Mavlink_Mission_Item, TIME, const char* >("ir_lp_recv", move(input_file_LP_recv.c_str()));
-		shared_ptr<dynamic::modeling::model> ir_plp_ach =
-			dynamic::translate::make_dynamic_atomic_model<Input_Reader_Mavlink_Mission_Item, TIME, const char* >("ir_plp_ach", move(input_file_PLP_ach.c_str()));
+	using state = logger::logger<logger::logger_state, dynamic::logger::formatter<TIME>, oss_sink_state>;
+	using log_messages = logger::logger<logger::logger_messages, dynamic::logger::formatter<TIME>, oss_sink_messages>;
+	using global_time_mes = logger::logger<logger::logger_global_time, dynamic::logger::formatter<TIME>, oss_sink_messages>;
+	using global_time_sta = logger::logger<logger::logger_global_time, dynamic::logger::formatter<TIME>, oss_sink_state>;
+	using info = logger::logger<logger::logger_info, dynamic::logger::formatter<TIME>, oss_sink_info>;
+	using logger_supervisor = logger::multilogger<state, log_messages, global_time_mes, global_time_sta, info>;
 
-		// The models to be included in this coupled model 
-		// (accepts atomic and coupled models)
-		dynamic::modeling::Models submodels_TestDriver = {
-			supervisor,
-			ir_landing_achieved,
-			ir_aircraft_state,
-			ir_pilot_takeover,
-			ir_lp_recv,
-			ir_plp_ach
-		};
+	auto start = hclock::now(); //to measure simulation execution time
 
-		dynamic::modeling::Ports iports_TestDriver = {	};
+	cadmium::dynamic::engine::runner<NDTime, logger_supervisor> r(test_driver, { TIME("00:00:00:000:000") });
+	r.run_until_passivate();
 
-		dynamic::modeling::Ports oports_TestDriver = {
-			typeid(o_LP_expired),
-			typeid(o_start_LZE_scan),
-			typeid(o_mission_complete),
-			typeid(o_land_requested),
-			typeid(o_fcc_command_velocity),
-			typeid(o_control_yielded),
-			typeid(o_notify_pilot),
-			typeid(o_fcc_command_hover)
-		};
+	auto elapsed = std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1>>>(hclock::now() - start).count();
+	cout << "Simulation took: " << elapsed << " seconds" << endl;
 
-		dynamic::modeling::EICs eics_TestDriver = {	};
-
-		// The output ports will be used to export in logging
-		dynamic::modeling::EOCs eocs_TestDriver = {
-			dynamic::translate::make_EOC<Supervisor_defs::o_LP_expired, o_LP_expired>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_start_LZE_scan, o_start_LZE_scan>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_mission_complete, o_mission_complete>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_land_requested, o_land_requested>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_fcc_command_velocity, o_fcc_command_velocity>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_control_yielded, o_control_yielded>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_notify_pilot, o_notify_pilot>("supervisor"),
-			dynamic::translate::make_EOC<Supervisor_defs::o_fcc_command_hover, o_fcc_command_hover>("supervisor")
-		};
-
-		// This will connect our outputs from our input reader to the file
-		dynamic::modeling::ICs ics_TestDriver = {
-			dynamic::translate::make_IC<iestream_input_defs<bool>::out, Supervisor_defs::i_landing_achieved>("ir_landing_achieved", "supervisor"),
-			dynamic::translate::make_IC<iestream_input_defs<message_aircraft_state_t>::out, Supervisor_defs::i_aircraft_state>("ir_aircraft_state", "supervisor"),
-			dynamic::translate::make_IC<iestream_input_defs<bool>::out, Supervisor_defs::i_pilot_takeover>("ir_pilot_takeover", "supervisor"),
-			dynamic::translate::make_IC<iestream_input_defs<message_landing_point_t>::out, Supervisor_defs::i_LP_recv>("ir_lp_recv", "supervisor"),
-			dynamic::translate::make_IC<iestream_input_defs<message_landing_point_t>::out, Supervisor_defs::i_PLP_ach>("ir_plp_ach", "supervisor")
-		};
-
-		shared_ptr<dynamic::modeling::coupled<TIME>> test_driver = make_shared<dynamic::modeling::coupled<TIME>>(
-			"test_driver", submodels_TestDriver, iports_TestDriver, oports_TestDriver, eics_TestDriver, eocs_TestDriver, ics_TestDriver
-		);
-
-		/*************** Loggers *******************/
-		out_messages = ofstream(out_messages_file);
-		struct oss_sink_messages {
-			static ostream& sink() {
-				return out_messages;
-			}
-		};
-
-		out_state = ofstream(out_state_file);
-		struct oss_sink_state {
-			static ostream& sink() {
-				return out_state;
-			}
-		};
-
-		out_info = ofstream(out_info_file);
-		struct oss_sink_info {
-			static ostream& sink() {
-				return out_info;
-			}
-		};
-
-		using state = logger::logger<logger::logger_state, dynamic::logger::formatter<TIME>, oss_sink_state>;
-		using log_messages = logger::logger<logger::logger_messages, dynamic::logger::formatter<TIME>, oss_sink_messages>;
-		using global_time_mes = logger::logger<logger::logger_global_time, dynamic::logger::formatter<TIME>, oss_sink_messages>;
-		using global_time_sta = logger::logger<logger::logger_global_time, dynamic::logger::formatter<TIME>, oss_sink_state>;
-		using info = logger::logger<logger::logger_info, dynamic::logger::formatter<TIME>, oss_sink_info>;
-		using logger_supervisor = logger::multilogger<state, log_messages, global_time_mes, global_time_sta, info>;
-
-		auto start = hclock::now(); //to measure simulation execution time
-
-		cadmium::dynamic::engine::runner<NDTime, logger_supervisor> r(test_driver, { TIME("00:00:00:000:000") });
-		r.run_until(TIME("00:01:00:000:000"));
-
-		auto elapsed = std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1>>>
-			(hclock::now() - start).count();
-		cout << "Simulation took: " << elapsed << " seconds" << endl;
-
-		test_set_enumeration++;
-	} while (filesystem::exists(i_base_dir + std::to_string(test_set_enumeration)));
 	return 0;
 }
